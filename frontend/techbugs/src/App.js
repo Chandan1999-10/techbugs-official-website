c26c8104479a1ccd8a769b485a32ef096c647fd8
Chandan1999-10 <58650645+Chandan1999-10@users.noreply.github.com>
2020-09-09 17:28:35 +0530

    nav bar
    Bootstrap navbar

--- frontend/techbugs/src/App.js
+++ frontend/techbugs/src/App.js
@@ -4,7 +4,35 @@ import './App.css';
 function App() {
   return (
     <div className="">
-    <a>Home</a>
+   <nav class="navbar navbar-expand-md navbar-dark">
+   <a className="navbar-brand">Techbugs</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Articles</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Gallery</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Reach US</a>
+      </li>
+      
+      </ul>
+      
+  </div>
+
+    </nav>
     
     </div>
   );
